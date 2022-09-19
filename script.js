b725b4f88374f7f62ffb8ce2342000704ca88a20
umangpincha <umangpincha@gmail.com>
2022-09-19 09:13:44 +0530

    repo initialized

---
+++ script.js
@@ -0,0 +1,83 @@
+// Not done
+// Q 7,8
+
+//TODO: Ans 1
+// document.querySelector(".side-bar .crayons-card .crayons-subtitle-2").innerText = "Umang Pincha"
+// document.querySelector(".side-bar .crayons-card .color-base-70").innerText = "I Write Code"
+
+
+//TODO: Ans 2
+// let nList  = document.querySelectorAll(".as-imagegrid-item-title");
+// a=[];
+// for (let element of nList) {
+//     a.push(element.innerText)
+// }
+// console.log(a);
+
+
+// TODO: ANS 3
+
+// Parent element
+// let p = document.querySelectorAll(".parent")
+// // New element <h3> My New FAQ </h3>
+// let node = document.createElement("h3");
+// node.textContent = 'My New FAQ';
+// //Append child
+// p[7].appendChild(node)
+
+// TODO: ANS 4
+// document.querySelector(".customer-support .one-tel-number").innerText = "+91 6366256689"
+
+// TODO: ANS 5
+// document.querySelector(".feature-column-carousel__content .feature-column-carousel__button .cta").innerText = "Check Out"
+
+// TODO: ANS 6
+// document.addEventListener("mouseover" , myFunc)
+// function myFunc() {
+//     document.querySelector(".searchinput-wrapper___18TsX .searchinput___zXLAR").style.backgroundColor = "red";
+// }
+
+// TODO: ANS 9
+// document.querySelector(".display-heading-1").style.fontFamily="monospace"
+// document.querySelector(".display-heading-1").style.color="#B1361E"
+
+// TODO: ANS 10
+// document.addEventListener("mouseover" , myFunc);
+// function myFunc(){
+//     document.querySelector(".btn-cta-big .login-btn-text").style.backgroundColor="red"
+// }
+
+// TODO:ANS 11
+// document.querySelector(".logo .icon").style.backgroundImage="url('https://ineuron.ai/images/ineuron-logo.png')"
+
+// TODO: ANS 12
+// document.querySelector(".js-repos-container .f4 .btn").style.backgroundColor="blue"
+
+// TODO: ANS 13
+// document.querySelector(".fl-col-content .fl-module .fl-module-content .fl-heading").innerText = "JSBOOTCAMP"
+
+
+// TODO: ANS 14
+// document.querySelector(".HotDealsAll__HotCampaignsEventsContainer__FK0V2 .HotDealsAll__Heading__2fIbe").style.fontSize="80px"
+
+// TODO: ANS 15
+// document.querySelectorAll(".ps-top .ps-title")[4].style.textAlign = "right"
+
+// TODO: ANS 16
+// document.querySelector(".section-title_title__VEDfK").textContent = "Start with Scratch"
+
+// TODO: ANS 17
+// let btn = document.querySelector(".btn-container");
+// let today = new Date();
+// btn.innerHTML =  today;
+
+// TODO: ANS 18
+// let footer = document.querySelector(".p-footer")
+// footer.style.backgroundColor = "yellow"
+
+// TODO: ANS 19
+// let img_url = document.querySelector(".navbar-brand")
+// console.log(img_url.children[0].src);
+ 
+// TODO: ANS 20
+// document.querySelector(".desc").style.color="orange"'
